c2c3f313f4953e251c70057487a6e04f77ef98a6
Shashank Bharti <shashank_24a12res620@iitp.ac.in>
2025-11-05 23:23:14 +0530

    Update package.json to use module type and modify server.js for dotenv configuration

--- 02 Fullstack Basic/Backend/server.js
+++ 02 Fullstack Basic/Backend/server.js
@@ -1,7 +1,46 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.configDotenv()
 
 const app = express()
 
+// app.get('/',(req,res)=>{
+//     res.send('Server Running')
+// })
+
+const port = process.env.PORT || 3000;
+
+app.get('/jokes',(req,res)=>{
+
+    const jokes = [
+        {
+            id:1,
+            title: 'Joke 1',
+            content:"Why don't scientists trust atoms? Because they make up everything!"
+        },
+        {
+            id:2,
+            title: 'Joke 2',
+            content:"Why did the scarecrow win an award? Because he was outstanding in his field!" 
+        },
+        {
+            id:3,
+            title: 'Joke 3',
+            content:"Why don't skeletons fight each other? They don't have the guts."
+        },
+        {
+            id:4,
+            title: 'Joke 4',
+            content:"What do you call fake spaghetti? An impasta!"
+        }
+    ]
+
+    res.send(jokes)
+
+})
+
+app.listen(port,()=>{
+    console.log(`App Running at http://localhost:${port}/jokes`);
+    
+})
